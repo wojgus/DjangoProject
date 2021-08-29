82f8932c2aad0f0b98d5d896b9b41fd146609b0f
wojgus <wojgus@tlen.pl>
2021-08-29 15:14:38 +0200

    Adding movie edit form

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from viewer.views import MoviesView, MovieCreateView
+from viewer.views import MoviesView, MovieCreateView, MovieUpdateView
 from viewer.models import Genre, Movie
 
 admin.site.register(Genre)
@@ -25,5 +25,8 @@ admin.site.register(Movie)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MoviesView.as_view(), name='index'),
-    path('movie/create', MovieCreateView.as_view(), name='movie_create')
+    path('movie/create', MovieCreateView.as_view(), name='movie_create'),
+    path('movie/update/<pk>', MovieUpdateView.as_view(),  name='movie_update'),
+
+
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from viewer.models import Movie
 from viewer.forms import MovieForm
@@ -13,25 +13,22 @@ class MoviesView(ListView):
     model = Movie
 
 
-class MovieCreateView(FormView):
+class MovieCreateView(CreateView):
     template_name = 'form.html'
     form_class = MovieForm
     success_url = reverse_lazy('movie_create')
 
-    def form_valid(self, form):
-        result = super().form_valid(form)
-        cleaned_data = form.cleaned_data
-        Movie.objects.create(
-            title=cleaned_data['title'],
-            genre=cleaned_data['genre'],
-            rating=cleaned_data['rating'],
-            released=cleaned_data['released'],
-            description=cleaned_data['description'],
-        )
-        LOGGER.warning('tu jestem 333')
-
-        return result
-
     def form_invalid(self, form):
         LOGGER.warning('User provided invalid data')
+        return super().form_invalid(form)
+
+
+class MovieUpdateView(UpdateView):
+    template_name = 'form.html'
+    form_class = MovieForm
+    success_url = reverse_lazy('index')
+    model = Movie
+
+    def form_invalid(self, form):
+        LOGGER.warning('User provided invalid data when updating')
         return super().form_invalid(form)
