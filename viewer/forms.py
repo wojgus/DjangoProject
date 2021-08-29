50eb15dc1367ade960810fe37c5ce69c50d4c311
wojgus <wojgus@tlen.pl>
2021-08-29 14:22:14 +0200

    Preparing MovieForm based on ModelForm

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,21 +1,19 @@
 from django.forms import (
-    Form, CharField, ModelChoiceField, IntegerField, DateField, Textarea
+    ModelForm, CharField, IntegerField,
 )
 import re
-from viewer.models import Genre
+from viewer.models import Movie
 from django.core.exceptions import ValidationError
 from viewer.validators import PastMonthField, capitalized_validator
-from logging import getLogger
-
-LOGGER = getLogger()
 
 
-class MovieForm(Form):
-    title = CharField(max_length=128)
-    genre = ModelChoiceField(queryset=Genre.objects)
-    rating = IntegerField(min_value=1, max_value=10)
-    released = DateField()
-    description = CharField(widget=Textarea, required=False)
+class MovieForm(ModelForm):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        title = CharField(validators=[capitalized_validator])
+        rating = IntegerField(min_value=1, max_value=10)
+        released = PastMonthField()
 
     def clean_description(self):
         initial = self.cleaned_data['description']
@@ -24,7 +22,6 @@ class MovieForm(Form):
 
     def clean(self):
         result = super().clean()
-        LOGGER.warning('tu jestem')
         if result['genre'].name == 'comedy' and result['rating'] > 7:
             self.add_error('genre', '')
             self.add_error('rating', '')
