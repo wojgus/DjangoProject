a62d8c7719397805cd3897da6f1b45c81bf6ca71
wojgus <wojgus@tlen.pl>
2021-08-29 12:08:34 +0200

    Adding create movie form : add title block to base template

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from viewer.views import MoviesView
+from viewer.views import MoviesView, MovieCreateView
 from viewer.models import Genre, Movie
 
 admin.site.register(Genre)
 admin.site.register(Movie)
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MoviesView.as_view(), name='index')
+    path('', MoviesView.as_view(), name='index'),
+    path('movie/create', MovieCreateView.as_view(), name='movie_create')
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,FormView
 
 from viewer.models import Movie
-
+from viewer.forms import MovieForm
 
 # Create your views here.
 class MoviesView(ListView):
     template_name = "movies.html"
     model = Movie
 
+class MovieCreateView(FormView):
+    template_name = 'form.html'
+    form_class = MovieForm
