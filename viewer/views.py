fb79da7e1affd9d378d15e9eb50ca3b097a0c9c8
wojgus <wojgus@tlen.pl>
2021-08-29 10:05:04 +0200

    Add class

--- viewer/views.py
+++ viewer/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 from viewer.models import Movie
 
 
 # Create your views here.
-class MoviesView(View):
-    def get(self, request):
-        return render(
-            request, template_name='movies.html',
-            context={
-                'movies': Movie.objects.all()
-            }
-        )
+class MoviesView(TemplateView):
+    template_name = "movies.html"
+    extra_context = {'movies': Movie.objects.all()}
