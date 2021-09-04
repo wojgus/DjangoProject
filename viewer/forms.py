b472bc0bebc08107ce77f2084cf24378e9c05ae0
wojgus <wojgus@tlen.pl>
2021-09-04 12:17:33 +0200

    Custom adding editing form template

--- viewer/forms.py
+++ viewer/forms.py
@@ -11,9 +11,14 @@ class MovieForm(ModelForm):
     class Meta:
         model = Movie
         fields = '__all__'
-        title = CharField(validators=[capitalized_validator])
-        rating = IntegerField(min_value=1, max_value=10)
-        released = PastMonthField()
+    title = CharField(validators=[capitalized_validator])
+    rating = IntegerField(min_value=1, max_value=10)
+    released = PastMonthField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
 
     def clean_description(self):
         initial = self.cleaned_data['description']
